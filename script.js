f0fe95223daf9ce2cc6aacbd8244902dbd94166e
Daud2020-png <mahtievd828@gmail.com>
2026-03-23 03:10:55 +0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,11 @@
+document.querySelectorAll('.faq-item').forEach((item) => {
+  item.addEventListener('toggle', () => {
+    if (!item.open) return;
+
+    document.querySelectorAll('.faq-item').forEach((other) => {
+      if (other !== item) {
+        other.open = false;
+      }
+    });
+  });
+});
